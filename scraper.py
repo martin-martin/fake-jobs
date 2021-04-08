7c60e8543d418507dda7d2189fd8049bc40a8bfa
Martin Breuss <martin@Martins-MacBook-Pro.local>
2021-04-08 19:25:03 +0200

    Update scraper to new HTML content

--- scraper.py
+++ scraper.py
@@ -9,14 +9,14 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='ResultsContainer')
 # print(results.prettify())
 
-job_elems = results.find_all('section', class_='card-content')
+job_elems = results.find_all('div', class_='card-content')
 
 for job_elem in job_elems:
     # Each job_elem is a new BeautifulSoup object.
     # You can use the same methods on it as you did before.
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
+    title_elem = job_elem.find('p', class_='title')
+    company_elem = job_elem.find('p', class_='company')
+    location_elem = job_elem.find('p', class_='location')
     print(title_elem.text.strip())
     print(company_elem.text.strip())
     print(location_elem.text.strip())
