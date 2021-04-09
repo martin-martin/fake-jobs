af66452580a822069c4aef6fcc7990538824da1d
Martin Breuss <martin@Martins-MacBook-Pro.local>
2021-04-09 12:11:50 +0200

    Clean up and add Python jobs

--- makepage.py
+++ makepage.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from random import randint
 from faker import Faker
 from slugify import slugify
-from snippets import job_html, base_html, bulma_html, bulma_job, job_detail_html
+from snippets import job_html, base_html, job_detail_html
+
 
 fake = Faker()
 Faker.seed(0)
@@ -14,25 +15,31 @@ job_loc = Path().cwd().joinpath("jobs")
 job_loc.mkdir(exist_ok=True)
 
 # customize the lorem generator to get some job talk going
-word_list = ['job','professional','Python',
-        'role','developer','Java',
-        'agile','SCRUM','oversee',
-        'Flask','Django','web application','dashboard' ]
+word_list = ['job','professional','Python', 'no experience', 'curious', 'willing to learn',
+        'role','developer','Java', 'support', 'collaborate', 'include', 'communities',
+        'agile','SCRUM','oversee', 'oversea', 'relocation', 'remote', 'distributed',
+        'Flask','Django','web application','dashboard','software developer', 'programs',
+        'build', 'coordinate', 'explore', 'growth opportunity', 'talented', 'motivated',
+        'grit', 'CSS', 'HTML', 'employ', 'discussions', 'inclusive', 'open-minded',
+        'detail-oriented', 'fast-growing', 'responsible tech', 'educational',
+        'environmentally friendly', 'teamwork', 'asset', 'company']
 
-for i in range(1000):
-    job = fake.job()
-    if "engineer" in job.lower():
-        # job_html.format(job_title, company_name, location, time)
+for i in range(100):
+    if i % 10 == 0:  # Create 10 jobs titled as Python jobs with software dev ipsum
+        job = "Python Developer"
         description = fake.paragraph(nb_sentences=randint(5, 10), ext_word_list=word_list)
-        slug_url = f"{slugify(job)}-{i}.html"
-        company = fake.company()
-        location = f"{fake.city()}, {fake.military_state()}"
-        date = fake.date()
-        jobs_list += bulma_job.format(job, company, location, date,
-                                      f"{BASE_URL}/jobs/{slug_url}")
-        with open(job_loc.joinpath(slug_url), "w") as detailpage:
-            details = job_detail_html.format(job, company, location, date, description)
-            detailpage.write(bulma_html.format(details))
+    else:
+        job = fake.job()
+        description = fake.paragraph(nb_sentences=randint(5, 10))
+    slug_url = f"{slugify(job)}-{i}.html"
+    company = fake.company()
+    location = f"{fake.city()}, {fake.military_state()}"
+    date = fake.date_between(start_date='-2m', end_date='today')
+    jobs_list += job_html.format(job, company, location, date,
+                                    f"{BASE_URL}/jobs/{slug_url}")
+    with open(job_loc.joinpath(slug_url), "w") as detailpage:
+        details = job_detail_html.format(job, company, location, date, description)
+        detailpage.write(base_html.format(details))
 
 with open("index.html", "w") as fout:
-    fout.write(bulma_html.format(jobs_list))
+    fout.write(base_html.format(jobs_list))
--- snippets.py
+++ snippets.py
@@ -1,102 +1,4 @@
 base_html = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Fake Python Jobs</title>
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Fake Python Jobs</a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" target="_blank" aria-current="page" href="https://www.realpython.com">Real Python</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-</nav>
-
-<div id="ResultsContainer">
-    {}
-</div>
-</body>
-</html>"""
-
-job_html = """<div class="card" style="width: 18rem;">
-<div class="card-body">
-<section class="card-content" data-jobid="4755ec59-d0db-4ce9-8385-b4df7c1e9f7c" onclick="MKImpressionTrackingMouseDownHijack(this, event)">
-<div class="flex-row">
-<div class="mux-company-logo thumbnail"></div>
-<div class="summary">
-<header class="card-header card-title">
-<h2 class="title"><a data-bypass="true">{0}</a></h2>
-</header>
-<div class="card-body">
-<div class="badge bg-light rounded-pill text-dark"><time datetime="{3}">{3}</time></div>
-<div class="company">
-<span class="name fs-3">{1}</span>
-<ul class="list-inline">
-</ul>
-</div>
-<div class="location">
-<span class="name">
-{2}
-</span>
-</div>
-</div>
-<div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-success btn-outline">Apply</button>
-  <button type="button" class="btn btn-primary btn-outline">Save</button>
-</div>
-</div>
-</div>
-</section>
-</div>
-</div>
-"""
-
-page_html = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Fake Python Jobs</title>
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Fake Python Jobs</a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" target="_blank" aria-current="page" href="https://www.realpython.com">Real Python</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-</nav>
-
-<div class="container">
-    {}
-</div>
-</body>
-</html>
-"""
-
-bulma_html = """<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -124,7 +26,7 @@ bulma_html = """<!DOCTYPE html>
 </html>
 """
 
-bulma_job = """<div class="column is-half">
+job_html = """<div class="column is-half">
 <div class="card">
   <div class="card-content">
     <div class="media">
